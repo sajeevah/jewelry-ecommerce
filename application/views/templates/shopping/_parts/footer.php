6da6a469aec34b3c33d659d13af7e71546ea377c
sajeevah <sa.hasith@gmail.com>
2021-06-06 02:27:54 +0530

    add instagram logo

--- application/views/templates/shopping/_parts/footer.php
+++ application/views/templates/shopping/_parts/footer.php
@@ -10,6 +10,7 @@
                         <ul class="social-icons">
                             <li>
                                 <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
                                 <a href="#"><i class="fa fa-twitter"></i></a>
                                 <a href="#"><i class="fa fa-linkedin"></i></a>
                             </li>
--- application/views/templates/shopping/view_blog_post.php
+++ application/views/templates/shopping/view_blog_post.php
@@ -72,9 +72,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
                         <ul class="list-inline">
                             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
                             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                             <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#"><i class="fa fa-behance"></i></a></li>
                         </ul>
                     </div>
                 </div>
